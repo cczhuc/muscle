8729682b135c6fba854d01e191bc7e14448e119a
Quci <xujiejiuxi@gmail.com>
2017-07-11 14:45:37 +0800

    完成需求：搜索范围的左右大小是不限制的

--- trunk/js/controllers/business/userList.js
+++ trunk/js/controllers/business/userList.js
@@ -3,17 +3,39 @@ angular.module('admin').controller('UserListCtrl',['$rootScope','$state','$http'
         var vm = this;
         vm.searchParams = $state.params;
 
-        portService.getUserList(vm.searchParams).then(function (res){
+        vm.searchParams.startAt = parseInt(vm.searchParams.startAt) || undefined;
+        vm.searchParams.endAt = parseInt(vm.searchParams.endAt) || undefined;
+
+        //需求：对输入范围的左边和右边的大小不限。  但是在发送数据的时候要对这些倒过来的参数进行处理
+        //vm.searchParams和$state.params和http请求的params是3个东西，要分清楚
+        //vm.tempParams是vm.searchParams的深拷贝，处理好时间和年龄的顺序后发送给后端
+        //深拷贝,直接等的话是浅拷贝，在页面上显示会有问题。不这样做会导致：eg：输入年龄 55和10，点击搜索后会显示10和55
+        vm.tempParams = JSON.parse(JSON.stringify(vm.searchParams));
+        if(vm.tempParams.startAt && vm.tempParams.endAt && vm.tempParams.startAt - 1 >= vm.tempParams.endAt) {
+            var tempAt = vm.tempParams.startAt;
+            //搜索按钮插件会对vm.searchParams.end + 86400000 -1,如果界面所以要反向操作
+            vm.tempParams.startAt = vm.tempParams.endAt - 86400000 + 1;
+            vm.tempParams.endAt = tempAt + 86400000 -1;
+        }
+
+        if(vm.tempParams.minAge && vm.tempParams.maxAge && vm.tempParams.minAge>vm.tempParams.maxAge) {
+            var tempAge = vm.tempParams.minAge;
+            vm.tempParams.minAge = vm.tempParams.maxAge;
+            vm.tempParams.maxAge = tempAge;
+        }
+
+        // console.log("vm.tempParams",vm.tempParams);
+
+        // 请求用户数据,不要用vm.searchParams，用中间层vm.tempParams
+        portService.getUserList(vm.tempParams).then(function (res){
             if (res.data.code===0) {
-                console.log(res);
+                // console.log(res);
                 vm.userList = res.data.data;
                 vm.total = res.data.total;
             } else {
                 $rootScope.alert(res.data.message);
             }
         });
-
-
 
         vm.freezeUser = function(id,type,status) {
             if (status === 0) {
