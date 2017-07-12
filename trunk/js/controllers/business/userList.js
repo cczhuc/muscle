7e4f82e6b4f4a1808fe45883cf50e6b041b98e6e
Quci <xujiejiuxi@gmail.com>
2017-07-12 23:51:10 +0800

    修改user路由页

--- trunk/js/controllers/business/userList.js
+++ trunk/js/controllers/business/userList.js
@@ -5,7 +5,6 @@ angular.module('admin').controller('UserListCtrl',['$rootScope','$state','$http'
 
         vm.searchParams.startAt = parseInt(vm.searchParams.startAt) || undefined;
         vm.searchParams.endAt = parseInt(vm.searchParams.endAt) || undefined;
-
         /***需求：对输入范围的左边和右边的大小不限。但是在发送数据的时候要对这些倒过来的参数进行处理
         //vm.tempParams是vm.searchParams的深拷贝，处理好时间和年龄的顺序后发送给后端
         //要深拷贝,直接等的话是浅拷贝，在页面上显示会有问题。不这样做会导致：eg：输入年龄 55和10，点击搜索后会显示10和55***/
